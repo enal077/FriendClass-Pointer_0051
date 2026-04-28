2365a403f1b441b10e1c0e2e742919abc917dd19
enal007 <zaenalajh4@gmail.com>
2026-04-28 15:48:59 +0700

    y

--- FriendFuncitionDua.cpp
+++ FriendFuncitionDua.cpp
@@ -25,8 +25,5 @@ void manusia::showNilaiPelajar(pelajar &x)
 
 int main()
 {
-    manusia budi;
-    pelajar pbudi;
-    budi.showNilaiPelajar(pbudi);
-    return 0;
+    // Lanjutan kode program...
 }
